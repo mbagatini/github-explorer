9db61fc7ebd216729a3506b2373de2d03fcf34cd
Morgana Bagatini <morganabagatini@gmail.com>
2021-01-23 09:59:15 -0300

    Criação do projeto

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Dashboard: React.FC = () => {
+  return <h3>Dashboard</h3>;
+};
+
+export default Dashboard;
---
+++ src/pages/Repository/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Repository: React.FC = () => {
+  return <h3>Repository</h3>;
+};
+
+export default Repository;
